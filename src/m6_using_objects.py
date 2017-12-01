a96534f116574225401814e432eef63e6681df40
mcintoap <mcintoap@rose-hulman.edu>
2017-12-01 14:16:40 -0500

    DONE

--- src/m6_using_objects.py
+++ src/m6_using_objects.py
@@ -5,8 +5,8 @@ This module lets you practice  ** using objects **, including:
   -- accessing their DATA via INSTANCE VARIABLES
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Drew McIntosh.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -14,9 +14,13 @@ import rosegraphics as rg
 def main():
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
+    two_circles()
+    circle_and_rectangle()
+    lines()
 
 
 def two_circles():
+
     """
     -- Constructs an rg.RoseWindow.
     -- Constructs and draws two rg.Circle objects on the window
@@ -27,12 +31,32 @@ def two_circles():
     -- Waits for the user to press the mouse, then closes the window.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function, per its doc-string above.
+    # DONE: 2. Implement this function, per its doc-string above.
     #    -- ANY two rg.Circle objects that meet the criteria are fine.
     #    -- File  COLORS.txt  lists all legal color-names.
     # Put a statement in   main   to test this function
     #    (by calling this function).
     # ------------------------------------------------------------------
+
+    window = rg.RoseWindow()
+
+    center_point = rg.Point(200,200)
+    radius = 10
+    circle = rg.Circle(center_point, radius)
+    circle.fill_color = 'green'
+    circle.outline_color = ('yellow')
+    circle.outline_thickness = '5'
+    circle.attach_to(window)
+
+
+    center2 = rg.Point(50,50)
+    radius2 = 25
+    circle2 = rg.Circle(center2,radius2)
+    circle2.outline_color = 'red'
+    circle2.attach_to(window)
+
+    window.render()
+    window.close_on_mouse_click()
 
 
 def circle_and_rectangle():
@@ -67,7 +91,7 @@ def circle_and_rectangle():
            150.0
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement this function, per its doc-string above.
+    # DONE: 3. Implement this function, per its doc-string above.
     #   -- ANY objects that meet the criteria are fine.
     # Put a statement in   main   to test this function
     #    (by calling this function).
@@ -75,6 +99,37 @@ def circle_and_rectangle():
     # IMPORTANT: Use the DOT TRICK to guess the names of the relevant
     #       instance variables for outline thickness, etc.
     # ------------------------------------------------------------------
+
+    window2 = rg.RoseWindow(500,500)
+
+    center = rg.Point(200,200)
+    radius = 25
+    circle = rg.Circle(center,radius)
+    circle.outline_thickness = '5'
+    circle.fill_color = 'blue'
+    circle.attach_to(window2)
+
+    point1 = rg.Point(300,300)
+    point2 =rg.Point(400,400)
+    rectangle = rg.Rectangle(point1,point2)
+    rectangle.outline_thickness = '5'
+    rectangle.attach_to(window2)
+
+    print('5')
+    print('blue')
+    print('(200,200)')
+    print('200')
+    print('200')
+    print('5')
+    print('none')
+    print('(350,350)')
+    print('350')
+    print('350')
+
+    window2.render()
+    window2.close_on_mouse_click()
+
+
 
 
 def lines():
@@ -102,6 +157,24 @@ def lines():
     # ------------------------------------------------------------------
     # TODO: 4. Implement and test this function.
     # ------------------------------------------------------------------
+    window3 = rg.RoseWindow()
+
+    point1 = rg.Point(0,0)
+    point2 = rg.Point(100,100)
+    line = rg.Line(point1,point2)
+    line.attach_to(window3)
+
+    point3 = rg.Point(200,200)
+    point4 = rg.Point(200,300)
+    line2 = rg.Line(point3,point4)
+    mid = line2.get_midpoint()
+    line2.attach_to(window3)
+    window3.render()
+    window3.close_on_mouse_click()
+    print(mid)
+    print('200')
+    print('250')
+
 
 
 # ----------------------------------------------------------------------
